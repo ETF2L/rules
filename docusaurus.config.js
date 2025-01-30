863f671842f10defddb16dee11dd3c5064c2c9b4
Radosław Serba <radoslaw@serba.ovh>
2025-01-30 23:08:11 +0100

    feat(*): add Algolia Search bar, resolve vulnerabilities

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,6 +10,38 @@ module.exports = {
   organizationName: 'etf2l',
   projectName: 'rules',
   themeConfig: {
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'HAKQVX9YDE',
+
+      // Public API key: it is safe to commit it
+      apiKey: '6e68bfe2cdf8438a8e2cfc7a7f62691c',
+
+      indexName: 'rules-etf2l',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: '/docs/', // or as RegExp: /\/docs\//
+      //   to: '/',
+      // },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
+    },
     navbar: {
       title: 'ETF2L Rules',
       logo: {
